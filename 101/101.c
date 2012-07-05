97445cce3466e9bc3a343dc9c0199cfb52a1f1b9
tonylu <tonylu@kkbox.com>
2012-07-05 18:59:55 +0800

    modified:   101/101.c 	modified:   102/102.c 	modified:   113/113.c 	modified:   272/272.c 	modified:   458/458.c     修改coding style

--- 101/101.c
+++ 101/101.c
@@ -5,7 +5,7 @@
 
 typedef struct Block_struct{
     int num;
-	int cur_addr;
+    int cur_addr;
     struct Block_struct *next;
     struct Block_struct *before;
 }Blocks;
@@ -25,23 +25,23 @@ void rollback(Blocks *node);
  * 找到後 回傳以指向該編號的指標
  */
 Blocks *find_node(int block_num){
-	int temp , check = 0;
-	Blocks *ptr = NULL;
-	for(temp = 0 ; temp < total ; temp++){
-		ptr = list[temp];
-		while(ptr != NULL){
-			if(ptr->num == block_num){
-				check = 1;
-				break;
-			}else{
-				ptr = ptr->next;
-			}
-		}
-		if(check == 1){
-			break;
-		}
-	}
-	return ptr;
+    int temp , check = 0;
+    Blocks *ptr = NULL;
+    for(temp = 0 ; temp < total ; temp++){
+        ptr = list[temp];
+        while(ptr != NULL){
+            if(ptr->num == block_num){
+                check = 1;
+                break;
+            }else{
+                ptr = ptr->next;
+            }
+        }
+        if(check == 1){
+            break;
+        }
+    }
+    return ptr;
 
 }
 
@@ -50,13 +50,13 @@ Blocks *find_node(int block_num){
  * 把該積木以上的積木全部搬回原來的位置(編號1 搬回1原本的位置)
  * */
 void rollback(Blocks *node){
-	Blocks *ptr = node->next;
-	while(ptr != NULL){
-		if(ptr->num != ptr->cur_addr){
-			move(ptr , ptr->num);
-		}
-		ptr = ptr->next;
-	}
+    Blocks *ptr = node->next;
+    while(ptr != NULL){
+        if(ptr->num != ptr->cur_addr){
+            move(ptr , ptr->num);
+        }
+        ptr = ptr->next;
+    }
 }
 
 
@@ -66,67 +66,67 @@ void rollback(Blocks *node){
  * list 是全域變數 Blocs *list[MAX]
  * */
 void move(Blocks *node , int addr_num){
-	Blocks *ptr = list[addr_num];
-	if(ptr != NULL){
-		while(ptr->next != NULL){
-			ptr = ptr->next;
-		}
-		ptr->next = node;
-		if(node->before != NULL){
-			node->before->next = NULL;
-		}else{
-			list[node->cur_addr] = NULL;
-		}
-		node->before = ptr;
-	}else{
-		list[addr_num] = node;
-		if(node->before != NULL){
-			node->before->next = NULL;
-		}
-		node->before = NULL;
-	}
+    Blocks *ptr = list[addr_num];
+    if(ptr != NULL){
+        while(ptr->next != NULL){
+            ptr = ptr->next;
+        }
+        ptr->next = node;
+        if(node->before != NULL){
+            node->before->next = NULL;
+        }else{
+            list[node->cur_addr] = NULL;
+        }
+        node->before = ptr;
+    }else{
+        list[addr_num] = node;
+        if(node->before != NULL){
+            node->before->next = NULL;
+        }
+        node->before = NULL;
+    }
 
-	while(node != NULL){
-		node->cur_addr = addr_num;
-		node = node->next;
-	}
+    while(node != NULL){
+        node->cur_addr = addr_num;
+        node = node->next;
+    }
 }
 
 
 void init_list(){
-	int i = 0 ;
-	for(i = 0 ; i < total ; i++){
-		Blocks *block = (Blocks *)malloc(sizeof(Blocks));
-		block->num = i;
-		block->cur_addr = i;
-		block->next = NULL;
-		block->before = NULL;
-		list[i] = block;
-	}
+    int i = 0 ;
+    for(i = 0 ; i < total ; i++){
+        Blocks *block = (Blocks *)malloc(sizeof(Blocks));
+        block->num = i;
+        block->cur_addr = i;
+        block->next = NULL;
+        block->before = NULL;
+        list[i] = block;
+    }
 }
 
 
 
 void display(){
     int temp , i;
-	Blocks *ptr = NULL;
-	for(i = 0 ; i <total ; i++){
-		printf("%d:" , i);
-		ptr = list[i];
-		while(ptr != NULL){
-			printf(" %d" , ptr->num);
-			ptr = ptr->next;
-		}
-		printf("\n");
-	}
+    Blocks *ptr = NULL;
+    for(i = 0 ; i <total ; i++){
+        printf("%d:" , i);
+        ptr = list[i];
+        while(ptr != NULL){
+            printf(" %d" , ptr->num);
+            ptr = ptr->next;
+        }
+        printf("\n");
+    }
 }
 
 int main(){
-	while(scanf("%d" , &total) == 1){
-		if((total >= 0) && (total < 25)){
-			break;
-		}
-	}
+    while(scanf("%d" , &total) == 1){
+        if((total >= 0) && (total < 25)){
+            break;
+        }
+    }
     init_list();
     char act[5] , kind[5];
     int a , b , cmd;
@@ -135,14 +135,14 @@ int main(){
             break;
         }else{
             scanf("%d %s %d" , &a , kind , &b);
-			if((a == b ) || ((a > total) && (b > total))){
-				continue;
-			}
+            if((a == b ) || ((a > total) && (b > total))){
+                continue;
+            }
             Blocks *a_block = find_node(a);
-			Blocks *b_block = find_node(b);
-			if(a_block->cur_addr == b_block->cur_addr){
-				continue;
-			}
+            Blocks *b_block = find_node(b);
+            if(a_block->cur_addr == b_block->cur_addr){
+                continue;
+            }
 
             if(act[0] == 'm'){
                 cmd = 10;
@@ -169,10 +169,10 @@ int main(){
                 case 22:
                     break;
             }
-           	move(a_block , b_block->cur_addr);
+            move(a_block , b_block->cur_addr);
         }
     }
-   	display();
+    display();
     return 0;
 }
 
